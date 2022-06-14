25c22aef81b0dd69fd3db585b6d1661f0129bf27
Thiago Silva <thiago.silva@avenuecode.com>
2022-06-14 09:44:11 -0300

    Adding tailwind

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,7 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    '@storybook/addon-postcss',
   ],
   framework: '@storybook/react',
   core: {
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter } from 'react-router-dom';
 import RenderRouter from './routes';
+import './styles/tailwind.css';
 import './styles/index.css';
 
 export const queryClient = new QueryClient({
